54a524941e740ff3c87b66004905a597948b5ef5
unknown <hyshenml>
2017-01-22 17:27:42 +0800

    'xt3'

--- xt.py
+++ xt.py
@@ -10,4 +10,8 @@ driver = webdriver.PhantomJS()
 driver.get('http://wcm.yundasys.com:65216/wcm/login.html')
 pass_img_url=driver.find_elements_by_id('imgCode')[0].get_attribute('src')
 print pass_img_url
-img_code=raw_input()
+img_code=raw_input('code:')
+name_input=driver.find_elements_by_id('username')
+pass_input=driver.find_elements_by_id('password')
+code_input=driver.find_elements_by_id('passcode')
+print name_input,pass_input,code_input
