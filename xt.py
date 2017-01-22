aed7036e54a3fbea5e90a9b4e6820e9fa63c2628
unknown <hyshenml>
2017-01-22 17:12:22 +0800

    'xt'

---
+++ xt.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+driver = webdriver.PhantomJS()
+driver.get('http://wcm.yundasys.com:65216/wcm/login.html')
+pass_img_url=driver.find_elements_by_id('imgCode')[0].get_attribute('src')
+print pass_img_url
+img_code=raw_input()
