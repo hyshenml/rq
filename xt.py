a0f0ac7f319acf6cbafcce1169fec311585b9715
unknown <hyshenml>
2017-01-24 16:09:58 +0800

    '1'

--- xt.py
+++ xt.py
@@ -6,12 +6,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import TimeoutException
+from time import sleep
 driver = webdriver.PhantomJS()
 driver.get('http://wcm.yundasys.com:65216/wcm/login.html')
 pass_img_url=driver.find_elements_by_id('imgCode')[0].get_attribute('src')
-print pass_img_url
-img_code=raw_input('code:')
 name_input=driver.find_elements_by_id('username')
 pass_input=driver.find_elements_by_id('password')
 code_input=driver.find_elements_by_id('passcode')
-print name_input,pass_input,code_input
+print pass_img_url
+img_code=raw_input('code:')
+code_input[0].send_keys('img_code')
+name=raw_input('name:')
+name_input[0].send_keys('name')
+passwd=raw_input('pass:')
+pass_input[0].send_keys('passwd')
+button=driver.find_elements_by_class_name('btn')[0]
+actions = ActionChains(driver)
+actions.click(button)
+actions.perform()
+sleep(5)
+t=driver.find_element_by_link_text(u'报工时')
+print t
